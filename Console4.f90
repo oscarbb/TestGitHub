c70c4dac03515dfac638e8a41607d1ecd5c19192
oscarbb <obalsells@gmail.com>
2017-06-05 18:35:57 +0000

    prueba hello world

---
+++ Console4.f90
@@ -0,0 +1,25 @@
+!  Console4.f90 
+!
+!  FUNCTIONS:
+!  Console4 - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: Console4
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program Console4
+
+    implicit none
+
+    ! Variables
+
+    ! Body of Console4
+    print *, 'Hello World'
+
+    end program Console4
+
