d7cdd288fd5d8a05fba41a2d44b957dc774afcea
oscarbb <obalsells@gmail.com>
2017-06-05 19:14:12 +0000

    test commit branch 1

---
+++ Console4.f90
@@ -0,0 +1,25 @@
+!  Console4.f90 
+!
+!  FUNCTIONS:
+!  Console4 - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: Console4
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program Console4
+
+    implicit none
+
+    ! Variables
+
+    ! Body of Console4
+    print *, 'Hello Woooorld mod master, branch tes1'
+
+    end program Console4
+
